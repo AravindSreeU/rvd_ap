04ee0e965dcc9e97429aea16f1987f8559db13d3
RVD_THE_THINKER <95367109+AravindSreeU@users.noreply.github.com>
2025-04-10 17:12:02 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,15 @@
-# your_module/__init__.py
+# rvd_ap/__init__.py
 
 from .rvd_ap import some_function, SomeClass
 
+# Define the public API for the package
 __all__ = ['some_function', 'SomeClass']
+
+# Meta information about the package
+__version__ = '0.1.0'
+__author__ = 'Aravind Sree U'
+__email__ = 'uaravindsree@gmail.com'
+__description__ = (
+    'My description will help you to pass time and understand my ability of writing codes'
+)
+__url__ = 'https://github.com/AravindSreeU/rvd_ap'
